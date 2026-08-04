c5ecb919c9143a0b59178c65d2a1acde0c7d796b
chiefebube <ebubechi.anyanwu@yahoo.com>
2026-08-04 22:12:22 +0100

    Remove fakeStoreAPI.js and integrate stock data fetching directly in index.js with a new renderStockTicker function for dynamic stock display.

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+import { getStockData } from './fakeStockAPI.js'
+
+setInterval(function(){
+  const stockData = getStockData()
+  renderStockTicker(stockData)
+}, 1500)
+
+let prevPrice =  null;
+
+function renderStockTicker(stockData) {
+    const stockDisplayName = document.getElementById('name')
+    const stockDisplaySymbol = document.getElementById('symbol')
+    const stockDisplayPrice = document.getElementById('price')
+    const stockDisplayPriceIcon = document.getElementById('price-icon')
+    const stockDisplayTime = document.getElementById('time')
+
+    const {name, sym, price, time} = stockData
+
+    const priceIcon = price > prevPrice ? 'green.svg' : price < prevPrice ? 'red.svg' : 'grey.svg'
+
+    const priceIconElement = document.createElement('img')
+    priceIconElement.src = `svg/${priceIcon}`
+    priceIconElement.alt = 'Price Direction Icon'
+
+    stockDisplayPriceIcon.innerHTML = ''
+    stockDisplayPriceIcon.appendChild(priceIconElement)
+
+    stockDisplayName.innerText = name
+    stockDisplaySymbol.innerText = sym
+    stockDisplayPrice.innerText = price
+    stockDisplayTime.innerText = time
+
+    prevPrice = price
+}
+
 /*
 App requirements:
  - The app should display the name, symbol, and 
@@ -21,12 +56,3 @@ Challenge:
   ⚠️ You will need to write code here in index.js and in
    fakeStockAPI.js.
 */
-
-function renderStockTicker(stockData) {
-    const stockDisplayName = document.getElementById('name')
-    const stockDisplaySymbol = document.getElementById('symbol')
-    const stockDisplayPrice = document.getElementById('price')
-    const stockDisplayPriceIcon = document.getElementById('price-icon')
-    const stockDisplayTime = document.getElementById('time')
-}
-
